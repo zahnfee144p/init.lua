6a549e600342456e4d39386a7ce43f192ece55e5
zahnfee144p <82705008+zahnfee144p@users.noreply.github.com>
2024-10-30 17:48:36 +0100

    Add files via upload

---
+++ lua/zahnfee/set.lua
@@ -0,0 +1,32 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true 
+
+vim.opt.wrap = false
+
+-- maybe use this
+-- vim.opt.swapfile = false
+-- vim.opt.backup = false
+
+-- useful, also look at this: https://github.com/kevinhwang91/nvim-fundo
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim//undodir"
+-- vim.opt.undofile = true
+-- vim.opt.undolevels = 1000
+-- vim.opt.undoreload = 10000
+
+-- maybe do this, depends on plugin responsiveness
+-- vim.opt.updatetime = 50 
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+-- needs plugin(s) to work properly
+-- vim.opt.signcolumn = "yes"
+-- don't know what this does exactly, may be useful
+-- vim.opt.isfname:append("@-@")
+
+vim.opt.colorcolumn = "80"
