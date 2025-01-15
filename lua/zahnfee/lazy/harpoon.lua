4df7bd43f610297da8131834d0eeba6b36570f3b
Oscar Fritzsche <oscar.fritzsche@dart-racing.de>
2025-01-15 12:26:16 +0100

    changed harpoon mapping to <leader>+[{(

--- lua/zahnfee/lazy/harpoon.lua
+++ lua/zahnfee/lazy/harpoon.lua
@@ -11,10 +11,10 @@ return {
 		{ "<leader>n", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
 		{ "<leader>N", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
 		{ "<leader>t", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
-		{ "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Go to harpoon file 1" },
-		{ "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Go to harpoon file 2" },
-		{ "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Go to harpoon file 3" },
-		{ "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Go to harpoon file 4" },
+		{ "<leader>+", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Go to harpoon file 1" },
+		{ "<leader>[", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Go to harpoon file 2" },
+		{ "<leader>{", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Go to harpoon file 3" },
+		{ "<leader>(", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Go to harpoon file 4" },
 	},
 }
 
