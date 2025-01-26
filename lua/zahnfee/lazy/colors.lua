c14af9a513e0bf7d0bb09cc60ec8becb99c32e95
Oscar Fritzsche <oscar.fritzsche@dart-racing.de>
2025-01-26 22:24:42 +0100

    feat: changed color scheme

--- lua/zahnfee/lazy/colors.lua
+++ lua/zahnfee/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -9,16 +9,21 @@ end
 return {
 
     {
-        "erikbackman/brightburn.vim",
-    },
-
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
+        "rose-pine/neovim",
+        name = "rose-pine",
         config = function()
-            ColorMyPencils()
+            require('rose-pine').setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
+
+            ColorMyPencils();
         end
+    },
+    {
+        "erikbackman/brightburn.vim",
     },
     {
         "ellisonleao/gruvbox.nvim",
@@ -72,20 +77,38 @@ return {
         end
     },
 
+
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        config = function ()
+            require('kanagawa').setup({
+                compile = true,             -- enable compiling the colorscheme
+                undercurl = true,            -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true},
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = true,         -- do not set background color
+                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                colors = {                   -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                theme = "wave",              -- Load "wave" theme when 'background' option is not set
+                background = {               -- map the value of 'background' option to a theme
+                    dark = "wave",           -- try "dragon" !
+                    light = "lotus"
                 },
             })
-
-            ColorMyPencils();
-        end
-    },
+            ColorMyPencils("kanagawa")
+        end,
+    }
 
 
 }
