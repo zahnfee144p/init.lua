a85eba2d4bf0b743c40cc087764a1f2cd3ac4558
Oscar Fritzsche <oscar.fritzsche@dart-racing.de>
2025-06-30 16:10:25 +0200

    add zig lsp, change colour scheme

--- lua/zahnfee/lazy/lsp.lua
+++ lua/zahnfee/lazy/lsp.lua
@@ -29,6 +29,7 @@ return {
                 "lua_ls",
                 "clangd",
                 "gopls",
+                "zls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
