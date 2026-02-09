99353dce0c03b2dcf5b86f6370036f42daca7bca
zahnfee <zahnfee@posteo.com>
2026-02-09 21:59:40 +0100

    feat: new key binds for jumping to errors; prev: [t  next:]t

--- lua/zahnfee/lazy/trouble.lua
+++ lua/zahnfee/lazy/trouble.lua
@@ -1,20 +1,22 @@
 vim.keymap.set("n", "<leader>t]", function()
-  require("trouble").next("diagnostics", { jump = true, skip_groups = true })
-  require("trouble").jump("diagnostics", { jump = true, skip_groups = true })
+    require("trouble").next("diagnostics", { jump = true, skip_groups = true })
+    require("trouble").jump("diagnostics", { jump = true, skip_groups = true })
 end)
-
 return {
     "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {
+        auto_close = true,
+        follow = false,
+    }, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
         {
-            "<leader>tt",
+            "<leader>tT",
             "<cmd>Trouble diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
         },
         {
-            "<leader>tT",
+            "<leader>tt",
             "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
             desc = "Buffer Diagnostics (Trouble)",
         },
@@ -37,6 +39,43 @@ return {
             "<leader>tQ",
             "<cmd>Trouble qflist toggle<cr>",
             desc = "Quickfix List (Trouble)",
+        },
+        {
+            "]t",
+            "<cmd>Trouble diagnostics next skip_groups=true jump=true filter.buf=0<cr>",
+            desc = "Next Trouble Item",
+        },
+        {
+            "[t",
+            "<cmd>Trouble diagnostics prev skip_groups=true jump=true filter.buf=0<cr>",
+            desc = "Previous Trouble Item (Current Buffer)",
+        },
+        {
+            "]T",
+            "<cmd>Trouble diagnostics next skip_groups=true jump=true<cr>",
+            desc = "Next Trouble Item",
+        },
+        {
+            "[T",
+            "<cmd>Trouble diagnostics prev skip_groups=true jump=true<cr>",
+            desc = "Previous Trouble Item (Current Buffer)",
+        },
+    },
+    win = {
+        modes = {
+            preview_float = {
+                mode = "diagnostics",
+                preview = {
+                    type = "float",
+                    relative = "editor",
+                    border = "rounded",
+                    title = "Preview",
+                    title_pos = "center",
+                    position = { 0, -2 },
+                    size = { width = 0.3, height = 0.3 },
+                    zindex = 200,
+                },
+            },
         },
     },
 }
